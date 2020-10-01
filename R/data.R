e75929275f3f217856f53259fc6d9a8e275237f4
stemangiola <mangiolastefano@gmail.com>
2020-10-01 17:28:08 +1000

    fix CHECKs

--- R/data.R
+++ R/data.R
@@ -6,3 +6,29 @@
 #' @source \url{https://bioconductor.org/packages/release/data/experiment/html/pasilla.html}
 #' @usage data(pasilla)
 "pasilla"
+
+
+#' de_gate_gates
+#'
+#' Stefano's gate list
+#'
+#' @format  A list of gates to use. Gates consist of x and y coordinates.
+#' @usage data(de_gate_gates)
+"de_gate_gates"
+
+
+#' cell_type_df
+#'
+#' Stefano's cell type df
+#'
+#' @format  A list of gates to use. Gates consist of x and y coordinates.
+#' @usage data(cell_type_df)
+"cell_type_df"
+
+#' BRCA
+#'
+#' Subset of the TCGA BRCA data set
+#'
+#' @format  A tidy data frame.
+#' @usage data(BRCA)
+"BRCA"
