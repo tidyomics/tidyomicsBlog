3f48ca4f9e903fbba48245c954db046cd84a5035
stemangiola <mangiolastefano@gmail.com>
2021-10-13 10:59:03 +1100

    update first post

--- dev/blog_commands.R
+++ dev/blog_commands.R
@@ -1,3 +1,5 @@
 options(blogdown.server.timeout = 600)
-blogdown:::serve_site(.site_dir = "blog", port=3073)
+blogdown:::serve_site(.site_dir = "blog", port=1313)
 blogdown::new_post()
+
+blogdown::build_site(build_rmd = TRUE)
